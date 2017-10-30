84b9740dba6226e1769864d0ac9b2a0fedd2255c
Pavel Trutman <pavel.trutman@cvut.cz>
2017-10-30 14:54:02 +0100

    Added some functions for computation with coordinates.

--- geoCV/coordinates.py
+++ geoCV/coordinates.py
@@ -12,21 +12,23 @@ class Coordinates:
 
   def a2h(points):
     """
-    Converts coordinates of points from affine coordinates to homogenous coordinates.
+    Converts coordinates of points from affine coordinates to homogeneous coordinates.
 
     Args:
       points (array): coordinates of points as columns
 
     Returns:
-      array: homogenous coordinates
+      array: homogeneous coordinates
     """
+
+    points = Coordinates.columnize(points)
 
     return np.vstack((points, np.ones((1, points.shape[1]))))
 
 
   def h2a(points):
     """
-    Converts coordinates of points from homogenous coordinates to affine coordinates.
+    Converts coordinates of points from homogeneous coordinates to affine coordinates.
 
     Args:
       points (array): coordinates of points as columns
@@ -35,7 +37,26 @@ class Coordinates:
       array: affine coordinates
     """
 
-    return (points/np.matlib.repmat(points[-1, :], points.shape[0], 1))[:-1, :]
+    points = Coordinates.columnize(points)
+
+    return Coordinates.normalize(points, homogeneous=True)[:-1, :]
+
+
+  def columnize(vectors):
+    """
+    Check and fix if vector is a column.
+
+    Args:
+      vectors (array): array of vectors to verify
+
+    Returns:
+      array: coilumnized vectors if required
+    """
+
+    if len(vectors.shape) == 1:
+      return Coordinates.onedim2col(vectors)
+    else:
+      return vectors
 
 
   def onedim2col(vector):
@@ -50,7 +71,7 @@ class Coordinates:
     """
 
     if len(vector.shape) != 1:
-      raise ValueError('Vector must be one dimensional.')
+      raise ValueError('Vector must be one dimensional, has ' + str(len(vector.shape)) + ' dimensions.')
 
     return vector[:, np.newaxis]
 
@@ -67,5 +88,43 @@ class Coordinates:
       array: calibrated points
     """
 
+    points = Coordinates.columnize(points)
+
     Kinv = np.linalg.inv(K)
     return Kinv.dot(points)
+
+
+  def vectorNorm(vectors):
+    """
+    Computes columnvise 2 norm.
+
+    Args:
+      vectors (array): vectors in columns
+
+    Returns:
+      array: norm of columns
+    """
+
+    vectors = Coordinates.columnize(vectors)
+
+    return np.linalg.norm(vectors, 2, axis=0)
+
+
+  def normalize(vectors, homogeneous=False):
+    """
+    Normalize the given vectors.
+
+    Args:
+      vectors (array): vectors to normalize as columns
+      homogeneous (bool): true if homogeneous coordinates given
+
+    Returns:
+      array: normalized vectors
+    """
+
+    vectors = Coordinates.columnize(vectors)
+
+    if homogeneous:
+      return vectors/np.matlib.repmat(vectors[-1, :], vectors.shape[0], 1)
+    else:
+      return vectors/np.matlib.repmat(Coordinates.vectorNorm(vectors), vectors.shape[0], 1)
--- tests/coordinates_test.py
+++ tests/coordinates_test.py
@@ -62,6 +62,29 @@ class TestCoordinates(unittest.TestCase):
         self.assertLessEqual(np.linalg.norm(a - geoCV.coordinates.h2a(h)), 1e-3)
 
 
+  def testColumnizeColumn(self):
+    """
+    Test of columnize function.
+    """
+
+    dims = [5, 10, 15, 25, 50, 100]
+    for i in range(len(dims)):
+      with self.subTest(i = i):
+        v = np.random.randn(dims[i], 1)
+        self.assertEqual((v.shape), geoCV.coordinates.columnize(v).shape)
+
+
+  def testColumnizeNotColumn(self):
+    """
+    Test of columnize function.
+    """
+
+    dims = [5, 10, 15, 25, 50, 100]
+    for i in range(len(dims)):
+      with self.subTest(i = i):
+        v = np.random.randn(dims[i])
+        self.assertEqual((dims[i], 1), geoCV.coordinates.columnize(v).shape)
+
   def testOnedim2col(self):
     """
     Test casting from 1D numpy array to 2D column vector.
@@ -83,6 +106,49 @@ class TestCoordinates(unittest.TestCase):
     points = np.array([[3.55289e+03, 2.08543e+03, 2.17548e+03, 2.274e+03], [1.21907e+03, 1.11655e+03, 8.37526e+02, 1.33706e+03], [1, 1, 1, 1]])
     pointsCal = np.array([[815.5, -651.96, -561.91, -463.39 ], [-329.18, -431.7, -710.724, -211.19], [1, 1, 1, 1]])
     self.assertLessEqual(np.linalg.norm(pointsCal - geoCV.coordinates.calibrate(points, K)), 1e-3)
+
+
+  def testVectorNorm(self):
+    """
+    Test norm computation of vectors.
+    """
+
+    dims = [5, 10, 15, 25, 50, 100]
+    for i in range(len(dims)):
+      with self.subTest(i = i):
+        v = np.random.randn(dims[i], dims[i])
+        vnorm = geoCV.coordinates.vectorNorm(v)
+
+        self.assertTrue(np.linalg.norm(np.linalg.norm(v, 2, axis=0) - vnorm) < 1e-3)
+
+
+  def testNormalizeAffine(self):
+    """
+    Test that vectors after normalization has norm 1.
+    """
+
+    dims = [5, 10, 15, 25, 50, 100]
+    for i in range(len(dims)):
+      with self.subTest(i = i):
+        v = np.random.randn(dims[i], dims[i])
+        vnorm = geoCV.coordinates.normalize(v)
+
+        self.assertTrue(all(geoCV.coordinates.vectorNorm(vnorm) - 1 < 1e-3))
+
+
+  def testNormalizeHomogeneous(self):
+    """
+    Test that vectors after normalization has last element 1.
+    """
+
+    dims = [5, 10, 15, 25, 50, 100]
+    for i in range(len(dims)):
+      with self.subTest(i = i):
+        v = np.random.randn(dims[i], dims[i])
+        vnorm = geoCV.coordinates.normalize(v)
+
+        self.assertTrue(all(vnorm[-1, :] - 1 < 1e-3))
+
 
 
 if __name__ == '__main__':
